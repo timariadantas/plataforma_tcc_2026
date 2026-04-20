d5b9f84db4b3ae3ef73e0f038ddfd51dffef509f
Maria Dantas <mariadantasti@gmail.com>
2026-04-20 05:56:16 -0300

    feat(product - domain): update product service

--- services/product-service/app/domain/entities/product.py
+++ services/product-service/app/domain/entities/product.py
@@ -3,23 +3,23 @@ import uuid
 
 class Product:
     def __init__(self, name, description, price, quantity):
-        self.id = str(uuid.uuid4())
-        self.name = name
+        self.id = str(uuid.uuid4()) 
+        self.name = name          
         self.description = description
         self.price = price
         self.quantity = quantity
-        self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
         self.active = True
 
     def to_dict(self):
         return {
-            "_id": self.id,
+            "id": self.id,                    
             "name": self.name,
             "description": self.description,
             "price": self.price,
             "quantity": self.quantity,
-            "created_at": self.created_at,
-            "updated_at": self.updated_at,
+            "created_at": self.created_at.isoformat(),
+            "updated_at": self.updated_at.isoformat(),
             "active": self.active
         }
