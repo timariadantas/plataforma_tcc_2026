f49156bf8c806ed6e271cf63b73923abbcad6603
Maria Dantas <mariadantasti@gmail.com>
2026-04-20 05:57:31 -0300

    feat(product - Infra): update product repository

--- services/product-service/app/infrastructure/repositories/product_repository.py
+++ services/product-service/app/infrastructure/repositories/product_repository.py
@@ -1,29 +1,66 @@
 from pymongo import MongoClient
+from infrastructure.logging.logger import logger
+from infrastructure.database.mongo_connection import get_database
+
+
+
 
 class ProductRepository:
 
     def __init__(self):
-        client = MongoClient("mongodb://localhost:27017")
-        db = client["product_db"]
-        self.collection = db["products"]
+      db = get_database()
+      self.collection = db["products"]
 
-    def save(self, product):
-        self.collection.insert_one(product.to_dict())
+    def save(self, product):    
+        try:                                                                  
+            logger.info("Saving product")
+            data = product.to_dict()
+            data["_id"] = data["id"]      ## Mongo precisa disso
+            self.collection.insert_one(data)
+        except Exception as e:
+            logger.error(f"Error saving product: {e}")
+            raise
 
-    def get_all(self):
-        return list(self.collection.find({"active": True}))
+    def get_all(self, page=1, limit=10):
+        try:
+            logger.info(f"Fetching produts page={page} limit={limit}")
+            skip = (page - 1) * limit
+            return list(
+                self.collection.find({"active": True})
+                .skip(skip)
+                .limit(limit)
+                )
+        except Exception as e:
+            logger.error(f"Error fetching products: {e}")
+            raise ValueError("DataBase Error") from e
+          
 
     def get_by_id(self, product_id):
-        return self.collection.find_one({"_id": product_id, "active": True})
+        logger.info("searching for product by ID")
+        product = self.collection.find_one({
+            "_id": product_id,
+            "active": True
+        })
 
+        if not product:
+            logger.warning(f"Product not found: {product_id}")
+            raise ValueError("Product not found")
+
+        return product
     def update(self, product_id, data):
-        self.collection.update_one(
+        result = self.collection.update_one(
             {"_id": product_id},
             {"$set": data}
         )
+        if result.matched_count == 0:
+            logger.warning(f"Update failed, product not found: {product_id}")
+            raise ValueError("Product not found")
 
     def delete(self, product_id):
-        self.collection.update_one(
+        result = self.collection.update_one(
             {"_id": product_id},
             {"$set": {"active": False}}
         )
+        if result.matched_count == 0:
+            logger.warning(f"Delete failed, product not found: {product_id}")
+            raise ValueError("Product not found")
