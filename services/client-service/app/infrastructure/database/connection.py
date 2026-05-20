aa22e63a67e47e0c0da5ea4a080c75a8443bcb2a
Maria Dantas <mariadantasti@gmail.com>
2026-05-20 19:19:52 -0300

    fix: update client service database connection

--- services/client-service/app/infrastructure/database/connection.py
+++ services/client-service/app/infrastructure/database/connection.py
@@ -1,7 +1,10 @@
 from venv import logger
 
 from dotenv import load_dotenv
-load_dotenv()
+from pathlib import Path
+
+env_path = Path(__file__).resolve().parents[5] / ".env"
+load_dotenv(env_path)
 
 import os
 import oracledb
@@ -14,11 +17,17 @@ logger = get_logger("DatabaseConnection")
 class DatabaseConnection:
 
     def __init__(self):
-        self.user = os.getenv("DB_USER")
-        self.password = os.getenv("DB_PASSWORD")
-        self.host = os.getenv("DB_HOST")
-        self.port = os.getenv("DB_PORT")
-        self.service_name = os.getenv("DB_SERVICE")
+        self.user = os.getenv("CLIENT_DB_USER")
+        self.password = os.getenv("CLIENT_DB_PASSWORD")
+        self.host = os.getenv("CLIENT_DB_HOST")
+        self.port = os.getenv("CLIENT_DB_PORT")
+        self.service_name = os.getenv("CLIENT_DB_SERVICE")
+        
+        print("CLIENT_DB_USER:", self.user)
+        print("CLIENT_DB_PASSWORD:", self.password)
+        print("CLIENT_DB_HOST:", self.host)
+        print("CLIENT_DB_PORT:", self.port)
+        print("CLIENT_DB_SERVICE:", self.service_name)
         
         if not all([self.user, self.password, self.host, self.port, self.service_name]):
             logger.error("Variáveis de ambiente não configuradas corretamente")
