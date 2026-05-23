be555a44a438d8566a411b606ec8b67d6d07e801
Maria Dantas <mariadantasti@gmail.com>
2026-05-23 09:34:18 -0300

    refactor: improve client DTO structure

--- services/client-service/app/api/dto/client_resquest_dto.py
+++ services/client-service/app/api/dto/client_resquest_dto.py
@@ -1,26 +1,25 @@
 from datetime import datetime
 
+
 class ClientRequestDto:
+
     def __init__(self, name, surname, email, birthdate):
         self.name = name
         self.surname = surname
         self.email = email
         self.birthdate = birthdate
-        
+
     @staticmethod
     def from_dict(data):
+
+        birthdate = datetime.strptime(
+            data.get("birthdate"),
+            "%Y-%m-%d"
+        ).date()
+
         return ClientRequestDto(
             name=data.get("name"),
             surname=data.get("surname"),
             email=data.get("email"),
-            birthdate=datetime.strptime(data.get("birthdate"), "%Y-%m-%d")
+            birthdate=birthdate
         )
-        
-
-
-
-
-
-
-
-
