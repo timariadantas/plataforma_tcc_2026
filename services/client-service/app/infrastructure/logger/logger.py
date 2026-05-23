703c20932996125f98e004edae52c6318e09584c
Maria Dantas <mariadantasti@gmail.com>
2026-05-23 09:36:03 -0300

    chore: cleanup dependencies and project structure

--- services/client-service/app/infrastructure/logger/logger.py
+++ services/client-service/app/infrastructure/logger/logger.py
@@ -1,40 +1,28 @@
 import logging
-import logging.handlers
 import os
-from queue import Queue
 
-log_queue = Queue()
-
-def setup_logger():
+def get_logger(name: str): 
     if not os.path.exists("logs"):
         os.makedirs("logs")
-    
+
+    logger = logging.getLogger(name)
+
+    if logger.hasHandlers():
+        return logger
+        
+    logger.setLevel(logging.INFO)
+
     formatter = logging.Formatter(
         "%(asctime)s | %(name)s | %(levelname)s | %(message)s"
     )
-    file_handler = logging.FileHandler("logs/app.log")
-    file_handler.setFormatter(formatter)
-    
-    console_handler = logging.StreamHandler()
-    console_handler.setFormatter(formatter)
-    
-    listener = logging.handlers.QueueListener(
-        log_queue,
-        file_handler,
-        console_handler
-    )
-    
-    listener.start()
-    return listener
 
+    console = logging.StreamHandler()
+    console.setFormatter(formatter)
 
-def get_logger(name: str):
-    logger = logging.getLogger(name)
-    
-    if not logger.handlers:
-        logger.setLevel(logging.INFO)
-    
-        queue_handler = logging.handlers.QueueHandler(log_queue)
-        logger.addHandler(queue_handler)
-    
+    file = logging.FileHandler("logs/app.log")
+    file.setFormatter(formatter)
+
+    logger.addHandler(console)
+    logger.addHandler(file)
+
     return logger
