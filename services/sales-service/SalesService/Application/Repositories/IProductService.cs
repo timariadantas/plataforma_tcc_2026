41ccf247a86c82f893129ed49cfcccd51e9ad7a5
Maria Dantas <mariadantasti@gmail.com>
2026-07-28 19:59:23 -0300

    feat(sales): integrate Product Service for product and stock validation

--- services/sales-service/SalesService/Application/Repositories/IProductService.cs
+++ services/sales-service/SalesService/Application/Repositories/IProductService.cs
@@ -2,7 +2,7 @@ namespace SalesService.Application.Repositories;
 public interface IProductService
 
 {
-    Task <bool> ProductsExists(string productId);
+    
     Task <int> GetStock(string productId);
     Task<decimal> GetPrice(string productId);
     Task DecreaseStock(string productId, int quantity);
--- services/sales-service/SalesService/Application/Services/ProductServiceClient.cs
+++ services/sales-service/SalesService/Application/Services/ProductServiceClient.cs
@@ -2,6 +2,7 @@ using System.Text.Json;
 using SalesService.Application.Repositories;
 using SalesService.Application.DTO.Response;
 using System.Net.Http.Json;
+using SalesService.Domain.Exceptions;
 
 namespace SalesService.Application.Services;
 
@@ -16,69 +17,69 @@ public class ProductServiceClient : IProductService
       _logger = logger;
    }
 
-   public async Task<bool> ProductsExists(string productId)
-   {
-      var response = await _http.GetAsync($"/products/{productId}");
-      return response.IsSuccessStatusCode;
-   }
-   public async Task<int> GetStock(string productId)
-{
-    var response = await _http.GetAsync($"/products/{productId}");
+   private async Task<ProductServiceResponse> GetProduct(string productId)
+    {
+        var response = await _http.GetAsync($"/internal/products/{productId}");
 
-    if (!response.IsSuccessStatusCode)
-        throw new Exception("Product not found");
-
-    var json = await response.Content.ReadAsStringAsync();
-
-    _logger.LogInformation("Product JSON: {Json}", json);
-
-    var product = JsonSerializer.Deserialize<ProductServiceResponse>(
-        json,
-        new JsonSerializerOptions
+        if (!response.IsSuccessStatusCode)
         {
-            PropertyNameCaseInsensitive = true
-        });
+            _logger.LogWarning("Product not found: {ProductId}", productId);
+            throw new NotFoundException("Product not found");
+        }
 
-    if (product == null)
-        throw new Exception("Invalid product response");
+        var json = await response.Content.ReadAsStringAsync();
 
-    return product.Quantity;
+        _logger.LogInformation("Product JSON: {Json}", json);
+
+        var product = JsonSerializer.Deserialize<ProductServiceResponse>(
+            json,
+            new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            });
+
+        if (product == null)
+            throw new ValidationException ("Invalid product response");
+
+        return product;
+    }
+
+
+    public async Task<int> GetStock(string productId)
+    {
+        var product = await GetProduct(productId);
+        return product.Quantity;
+    }
+
+ 
+    public async Task<decimal> GetPrice(string productId)
+    {
+        var product = await GetProduct(productId);
+        return product.Price;
+    }
+
+    public async Task DecreaseStock(string productId, int quantity)
+    {
+        var body = new
+        {
+            quantity
+        };
+
+        var response = await _http.PatchAsJsonAsync(
+            $"/internal/products/{productId}/decrease-stock",
+            body
+        );
+
+        if (!response.IsSuccessStatusCode)
+        {
+            _logger.LogError(
+                "Error decreasing stock for product {ProductId}",
+                productId
+            );
+
+            throw new ValidationException ("Error decreasing stock");
+        }
+    }
+
+  
 }
-   public async Task<decimal> GetPrice(string productId)
-   {
-      var response = await _http.GetAsync($"/products/{productId}");
-
-      if (!response.IsSuccessStatusCode)
-         throw new Exception("Product not found");
-
-      var json = await response.Content.ReadAsStringAsync();
-
-      _logger.LogInformation("Product JSON: {Json}", json);
-      var product = JsonSerializer.Deserialize<ProductServiceResponse>(
-         json,
-      new JsonSerializerOptions
-         {
-            PropertyNameCaseInsensitive = true
-         });
-
-      if (product == null)
-         throw new Exception("Invalid product response");
-      
-      return product.Price;
-   }
-
-   public async Task DecreaseStock(string productId, int quantity)
-   {
-      var body = new
-      {
-         quantity = quantity
-      };
-
-      var response = await _http.PatchAsJsonAsync($"/products/{productId}/decrease-stock", body);
-
-      if (!response.IsSuccessStatusCode)
-         throw new Exception("Error decreasing stock");
-   }
-}
-
-
